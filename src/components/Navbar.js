3cb8d1607bb9e2f2b2cbfeef91c1c82913d50849
Nafis.3000 <nafis.3000@gmail.com>
2023-01-22 15:38:49 -0600

    navbar outline

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,146 @@
-import React, { Component } from 'react'
-import { motion } from 'framer-motion'
+import React, { useCallback, useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
+import { FaLinkedin, FaGithub, FaFreeCodeCamp } from "react-icons/fa";
+import { motion, useScroll } from "framer-motion";
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <motion.div className="flex w-full h-19 shadow-2xl z-[100] bg-slate-200">
-        Navbar
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const handleNav = () => {
+    setShow(!show);
+  };
+
+  return (
+    <motion.div className="fixed w-full h-19 shadow-2xl z-[100] bg-slate-100">
+      <motion.div className="flex justify-between items-center w-full h-full pr-2 2xl:px-16">
+        <Image src="/NA.png" alt="/" width="75" height="50" className="" />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/#home">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/#projects">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                My Work
+              </li>
+            </Link>
+            <Link href="/#about">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                My Story
+              </li>
+            </Link>
+            <Link href="/#skills">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/#contact">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div onClick={handleNav} className="md:hidden cursor-pointer">
+          <AiOutlineMenu size={25} className="text-2xl" />
+        </div>
       </motion.div>
-    );
-  }
-}
 
-export default Navbar
+      <div
+        className={
+          show ? "md:hidden fixed left-0 top-0 w-full h-full bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            show
+              ? "fixed left-0 top-0 w-[65%] sm:w-[60%] md:w-[45%] h-full bg-slate-100 p-1 ease-in duration-500"
+              : "fixed left-[-100%] top-0  p-1 ease-in duration-500"
+          }
+        >
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Image
+                src="/NA.png"
+                alt="/"
+                width="60"
+                height="30"
+                className="rounded"
+              />
+              <div
+                onClick={handleNav}
+                className="rounded shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              >
+                <AiOutlineClose size={15} />
+              </div>
+            </div>
+            <div className="border-b border-slate-400 my-4 text-center">
+              <p>Let's build something together.</p>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase ml-3">
+              <Link href="/#home" onClick={handleNav}>
+                <li className="py-4 text-sm border-b border-gray-300">Home</li>
+              </Link>
+              <Link href="/#projects" onClick={handleNav}>
+                <li className="py-4 text-sm border-b border-gray-300">
+                  My Work
+                </li>
+              </Link>
+              <Link href="/#about" onClick={handleNav}>
+                <li className="py-4 text-sm border-b border-gray-300">
+                  My Story
+                </li>
+              </Link>
+              <Link href="/#skills" onClick={handleNav}>
+                <li className="py-4 text-sm border-b border-gray-300">
+                  Skills
+                </li>
+              </Link>
+              <Link href="/#contact" onClick={handleNav}>
+                <li className="py-4 text-sm border-b border-gray-300">
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div className="mt-8">
+              <p className="uppercase tracking-widest text-sky-600 ml-2">
+                Let's Connect
+              </p>
+              <div className="flex items-center justify-around my-6 w-full sm:w[80%] text-3xl">
+                <div className="rounded shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duratation-300">
+                  <Link
+                    href="https://www.linkedin.com/in/nafis-anwar-1a6a0a141/"
+                    target="_blank"
+                  >
+                    <FaLinkedin />
+                  </Link>
+                </div>
+                <div className="rounded shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duratation-300">
+                  <Link href="https://github.com/Nafis3000" target="_blank">
+                    <FaGithub />
+                  </Link>
+                </div>
+                <div className="rounded shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duratation-300">
+                  <Link href="mailto:nafis.3000@gmail.com" target="_blank">
+                    <AiOutlineMail />
+                  </Link>
+                </div>
+                <div className="rounded shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duratation-300">
+                  <Link
+                    href="https://www.freecodecamp.org/fcc12f59cba-6269-44ac-9407-65b90082c113"
+                    target="_blank"
+                  >
+                    <FaFreeCodeCamp />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Navbar;
