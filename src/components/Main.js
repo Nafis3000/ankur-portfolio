17751a789ea64db3655289b62188072855e32a36
Nafis.3000 <nafis.3000@gmail.com>
2023-01-23 22:05:02 -0600

    prefer dark fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,7 +25,7 @@ const Main = () => {
     <>
       <motion.div
         id="home"
-        className="w-full md:h-screen p-2 flex items-center py-8 dark:bg-zinc-900"
+        className="w-full md:h-screen p-2 flex items-center py-8 bg-zinc-900"
       >
         <motion.div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
           <motion.div
@@ -34,11 +34,11 @@ const Main = () => {
             initial={"hidden"}
             whileInView={"visible"}
           >
-            <motion.h2 className="text-gray-200 text-6xl tracking-widest pt-8 dark:text-slate-100 text-center">
+            <motion.h2 className="text-6xl tracking-widest pt-8 text-slate-100 text-center">
                 <motion.span>Hello, my name is </motion.span>
                 <motion.span className="bg-lime-400 text-black">Ankur</motion.span>
             </motion.h2>
-            <motion.h2 className="text-gray-200 text-6xl tracking-widest pb-8 dark:text-slate-100 text-center">
+            <motion.h2 className=" text-6xl tracking-widest pb-8 text-slate-100 text-center">
               I <motion.span className="text-red-400">design</motion.span> and <motion.span className="text-blue-300">develop</motion.span>.
             </motion.h2>
             <motion.p
