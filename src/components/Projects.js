2863da970d9bfaf2fcc2eed5f923222862003ebb
Nafis.3000 <nafis.3000@gmail.com>
2023-01-25 22:24:31 -0600

    lime-cyan gradient

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,15 +10,15 @@ export default function Projects() {
     <>
       <motion.div
         id="projects"
-        className="w-full md:h-screen p-2 flex items-center py-12 bg-zinc-800"
+        className="w-full md:h-full p-2 flex items-center py-12 bg-gradient-to-b from-lime-300 to-cyan-300 "
       >
         <motion.div className="w-full max-w-[1240px] m-auto flex flex-col gap-10 justify-evenly">
-          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer w-3/5">
+          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer md:w-3/5 md:ml-10 h-96">
             <motion.div></motion.div>
           </motion.div>
-          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer w-3/5 self-end"></motion.div>
-          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer w-3/5"></motion.div>
-          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer w-3/5 self-end"></motion.div>
+          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer md:w-3/5 md:mr-10 md:self-end h-96"></motion.div>
+          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer md:w-3/5 md:ml-10 h-96"></motion.div>
+          <motion.div className="card p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer md:w-3/5 md:mr-10 md:self-end h-96"></motion.div>
         </motion.div>
       </motion.div>
     </>
